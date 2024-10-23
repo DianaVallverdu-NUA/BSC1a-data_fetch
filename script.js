5d423a7ab9edeffd49b356f2492074426c663fdd
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-10-23 16:16:42 +0100

    basic start

---
+++ script.js
@@ -0,0 +1,46 @@
+const image = document.getElementById("image");
+
+/**
+ * gets a random dog form the api
+ * @returns json with image url
+ */
+async function getRandomDog() {
+  //random url
+  const url = "https://dog.ceo/api/breeds/image/random";
+
+  try {
+    //get response
+    const response = await fetch(url);
+
+    //look for errors
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    //transform response to json -{message: `url`; status: 'success'}
+    const json = await response.json();
+    return json;
+
+    //log error if error
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+/**
+ * on success update image source what's stored in the message
+ * @param {Object} result {message: `url`, status: `status`}
+ */
+function onSuccess(result) {
+  image.src = result.message;
+}
+
+/**
+ * if an error is thrown -> display in console
+ * @param {Error} error 
+ */
+function onError(error) {
+  console.error(error);
+}
+
+getRandomDog().then(onSuccess, onError);
