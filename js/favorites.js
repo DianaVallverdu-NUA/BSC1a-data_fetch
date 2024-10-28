9aeea01145ad32a93b802e10c0a461253ba0f54e
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-10-28 12:16:49 +0000

    remove / download as icons

--- js/favorites.js
+++ js/favorites.js
@@ -39,11 +39,20 @@ function createNewFavouriteDog() {
 
   //buttons
   const downloadButton = document.createElement("button");
-  downloadButton.innerHTML = "Download";
+  // downloadButton.innerHTML = "Download";
+  downloadButton.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>';
+  downloadButton.classList.add("button");
+  downloadButton.classList.add("button-small");
+
   downloadButton.onclick = downloadImage;
 
   const removeButton = document.createElement("button");
-  removeButton.innerHTML = "Remove";
+  removeButton.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
+  removeButton.classList.add("button");
+  removeButton.classList.add("button-small");
+
   removeButton.onclick = removeFavorite;
 
   //containing div
