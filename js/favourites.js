f1d068925ac02976a85c16e030b2e8c6cd51de33
Diana0604 <diana.vallverdu@gmail.com>
2024-10-29 15:16:02 +0000

    prevent favourites from repeating

--- js/favourites.js
+++ js/favourites.js
@@ -7,10 +7,19 @@ const addFavouriteButton = document.getElementById("add-favourite-button");
 //current image stores image currently displayed - updated in database.js onFetchRandomDog function
 let currentImage = undefined;
 
+//list of all favourites used to manage repetitions
+let favouritesList = [];
+
 /**
  * Create new image element and append it to the favorites div
  */
 function onAddFavouriteButtonClick() {
+    //check currently displayed image not in list already
+    if (favouritesList.includes(currentImage)) return;
+
+    //add currently displayed image to favourites list
+    favouritesList.push(currentImage);
+
     //create newImage and update source
     const newImage = document.createElement("img");
     newImage.src = currentImage;
